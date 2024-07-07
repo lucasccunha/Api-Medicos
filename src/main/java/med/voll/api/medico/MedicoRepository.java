9a550036e353ee5a6dd8523bf603db16de2a7cf5
Lucas Cunha <lucas71cunha@gmail.com>
2024-07-07 18:06:58 -0300

    Nova mensagem

---
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import aj.org.objectweb.asm.commons.Remapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long>{
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+}
